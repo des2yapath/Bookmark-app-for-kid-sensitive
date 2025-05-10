7553bcaba8d82e03095fbb54e13ba9ae587370f8
Divyansh <divyanshahuja2017@gmail.com>
2025-05-10 15:25:37 +0530

    Initial commit

---
+++ src/main/java/com/semanticsquare/bookmarkIO/entities/BookmarkIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.semanticsquare.bookmarkIO.entities;
+
+/**
+ *
+ * @author divya
+ */
+public class BookmarkIO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
